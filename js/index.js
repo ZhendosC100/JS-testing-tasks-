3b96d6e6b33bcd087fa6c263d43f0f89244435ef
Yauhen Tretsyak <zhendos@tut.by>
2019-06-13 00:20:54 +0200

    add some parts of lesson 4

--- js/index.js
+++ js/index.js
@@ -5,21 +5,43 @@ let money = +prompt('Ваш месячный доход? ', ''),
     mission = '80000', 
     period = 12,
     budgetDay,
+    // accumulatedMonth, //Накопления за месяц
     a_1 = prompt('Какие обязательные ежемесячные расходы у вас есть?', ''),
     a_2 = prompt('Какие обязательные ежемесячные расходы у вас есть?', ''),
-    b_1 = +prompt('Во сколько это обойдется', ''),
-    b_2 = +prompt('Во сколько это обойдется', ''),
-    bugetMonth = money - (b_1 + b_2);
+    expensesMonth_1 = +prompt('Во сколько это обойдется', ''),
+    expensesMonth_2 = +prompt('Во сколько это обойдется', ''),
+    bugetMonth = money - (expensesMonth_1 + expensesMonth_2);
 
     budgetDay = Math.floor(bugetMonth/30);
 
 console.log(addExpenses.split(', '));
 
+    // Возвращает значение: все расходы за месяц
+let getExpensesMonth = () =>{ 
+    console.log(expensesMonth_1 + expensesMonth_2);
+};
+    getExpensesMonth();
+
+    //Возвращает: Накопления за месяц (Доходы минус расходы)
+let getAccumulatedMonth = () => { 
+    // console.log(`Накопления за месяц ${money - (expensesMonth_1 + expensesMonth_2)}`);
+    return money - (expensesMonth_1 + expensesMonth_2);
+};
+let accumulatedMonth =  getAccumulatedMonth();
+console.log(accumulatedMonth); // Накопления за месяц
+
+    // Подсчитывает за какой период будет достигнута цель
+let getTargetMonth = () => {
+    console.log( Math.ceil(mission/bugetMonth) );
+};
+getTargetMonth();
+
+
 console.log(deposit);
 console.log(`bugetMonth ${bugetMonth}`);
 console.log(`budgetDay ${budgetDay}`);
 
-console.log( Math.ceil(mission/bugetMonth) );
+
 
 if(budgetDay < 0){
     console.log('Что то пошло не так');
